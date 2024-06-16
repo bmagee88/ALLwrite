a7fe7c8d35b8b736e56658547e3ce7ab266b843f
Brian <53064162+bmagee88@users.noreply.github.com>
2024-06-16 15:20:20 +0300

    incorporate basicAvatart into controlpanel

--- frontend/src/common/components/ProfileMenu/ProfileMenu.tsx
+++ frontend/src/common/components/ProfileMenu/ProfileMenu.tsx
@@ -3,21 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Dropdown } from "react-bootstrap";
 import Box from "@mui/material/Box";
 import ProfileMenuItems from "./ProfileMenuItems/ProfileMenuItems";
+import BasicAvatar from "../user/BasicAvatar/BasicAvatar";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 const ProfileMenu = () => {
+  const user = useSelector((state: RootState) => state.user.user);
   return (
     <Box className='row justify-content-end'>
       <Dropdown>
         <Dropdown.Toggle
           variant=''
           id='dropdown-basic'>
-          <Box
+          <BasicAvatar
+            firstName={user?.firstname || "0"}
+            lastName={user?.lastname || "0"}
+          />
+          {/* <Box
             component='img'
             sx={{ borderRadius: "50%", width: "2.5rem", height: "auto" }}
             src={AnonUserImage}
             alt=''
             width='25'
-          />
+          /> */}
         </Dropdown.Toggle>
         <Dropdown.Menu>
           <ProfileMenuItems />
