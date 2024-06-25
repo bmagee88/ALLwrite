d3dc6f3756d80a071a6ea220dcd3347646267fcb
Brian <53064162+bmagee88@users.noreply.github.com>
2024-06-25 13:08:12 +0300

    added user, page, cover, and read entity

---
+++ backend/src/entities/User.entity.ts
@@ -0,0 +1,23 @@
+class User {
+  username;
+  firstname;
+  lastname;
+  email;
+  password;
+
+  constructor(
+    username: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string
+  ) {
+    this.username = username;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export default User;
