f2b0369eccdf2043aa282d7e9f10afe36dd13fdc
Brian <53064162+bmagee88@users.noreply.github.com>
2024-06-25 12:57:06 +0300

    change file ext to ts

--- backend/src/services/NodeService.ts
+++ backend/src/services/NodeService.ts
@@ -1,6 +1,8 @@
-const bcrypt = require("bcryptjs");
+import { compare } from "bcryptjs";
+import { Client, QueryResult } from "pg";
+import User from "../entities/User.entity";
 
-async function getChoices(client, parent_id, limit) {
+export async function getChoices(client: Client, parent_id: string, limit: string) {
   // console.log("pid, limit", parent_id, limit);
   const res = await client.query(
     `select * from page where parent_id = ${parent_id} order by RANDOM() limit ${limit}`
@@ -9,7 +11,8 @@ async function getChoices(client, parent_id, limit) {
   return res.rows;
 }
 
-async function createCover(client, cover) {
+export async function createCover(client: Client, cover) {
+  // should be of type Cover
   // console.log("service: cover", cover);
   const res = await client.query(
     `insert into covers (title, author, genre, summary, first_page) values ('${cover.title}', '${cover.author}', '${cover.genre}', '${cover.summary}', ${cover.first_page});`
@@ -18,7 +21,7 @@ async function createCover(client, cover) {
   return res.rows;
 }
 
-async function login(client, username, password) {
+export async function login(client: Client, username: string, password: string) {
   console.log("username", username);
   console.log("password", password);
   const empty_result = {
@@ -29,7 +32,7 @@ async function login(client, username, password) {
   if (testing) {
     console.log("testing");
 
-    const userInfo = await client.query(
+    const userInfo: QueryResult<any> = await client.query(
       `select user_id, username, firstname, lastname, email from user_profile where username = '${username}' and test_text_password = '${password}'`
     );
     console.log("userInfo:", userInfo.rows[0]);
@@ -41,58 +44,40 @@ async function login(client, username, password) {
       console.log("userInfo", userInfo.rows[0]);
       return userInfo.rows[0];
     }
-
-    // try {
-    //   const text_password = await client.query(
-    //     `SELECT test_text_password FROM user_profile WHERE username = '${username}';`
-    //   );
-
-    //   if (Array.isArray(text_password.rows) && text_password.rows.length === 0) {
-    //     console.log("Array is empty or length is 0, returning empty result");
-    //     return empty_result;
-    //   } else {
-    //   console.log("password", text_password.rows[0]);
-    //   if
-    //     return text_password.rows;
-    //   }
-    // } catch (error) {
-    //   console.error("Error querying database:", error);
-    //   return empty_result; // Handle error case by returning empty result
-    // }
   }
   // validate password
   // get username stored hashed password
-  const res = await client
-    .query(`select password from user_profile where username = '${username}';`)
-    .then((get_hashed_pass_query_result) => {
-      console.log("query results", get_hashed_pass_query_result);
-      if (
-        Array.isArray(get_hashed_pass_query_result) &&
-        get_hashed_pass_query_result.length === 0
-      ) {
-        console.log("array empty or length 0, returning empty result");
-        return empty_result;
-      }
-      let hashed_password_from_db = get_hashed_pass_query_result.rows[0].password;
-      if (bcrypt.compare(password, hashed_password_from_db)) {
-        const userInfo = client.query(
-          `select user_id, username, firstname, lastname, email from user_profile where username = '${username}' and password = '${hashed_password_from_db}'`
-        );
-        console.log("userInfo:", userInfo);
-        return userInfo;
-      } else {
-        console.log("password bad, returning empty result");
-        return empty_result;
-      }
-    })
-    .catch((err) => {
-      console.log("something went wrong:", err);
-    });
-  console.log(res.rows);
-  return res.rows;
+  // const res: QueryResult<any> = await client
+  //   .query(`select password from user_profile where username = '${username}';`)
+  //   .then((get_hashed_pass_query_result) => {
+  //     console.log("query results", get_hashed_pass_query_result);
+  //     if (
+  //       Array.isArray(get_hashed_pass_query_result) &&
+  //       get_hashed_pass_query_result.length === 0
+  //     ) {
+  //       console.log("array empty or length 0, returning empty result");
+  //       return empty_result;
+  //     }
+  //     let hashed_password_from_db = get_hashed_pass_query_result.rows[0].password;
+  //     if (compare(password, hashed_password_from_db)) {
+  //       const userInfo = client.query(
+  //         `select user_id, username, firstname, lastname, email from user_profile where username = '${username}' and password = '${hashed_password_from_db}'`
+  //       );
+  //       console.log("userInfo:", userInfo);
+  //       return userInfo;
+  //     } else {
+  //       console.log("password bad, returning empty result");
+  //       return empty_result;
+  //     }
+  //   })
+  //   .catch((err:Error) => {
+  //     console.log("something went wrong:", err);
+  //   });
+  // console.log(res.rows);
+  // return res.rows;
 }
 
-async function isUsernameTaken(client, username) {
+export async function isUsernameTaken(client: Client, username: string) {
   console.log("given username", username);
   const result = await client.query(
     `select username from user_profile where username = '${username}';`
@@ -101,8 +86,10 @@ async function isUsernameTaken(client, username) {
   return Array.isArray(result.rows) && result.rows.length !== 0;
 }
 
-async function createUser(client, user) {
-  const resUser = await client
+export async function createUser(client: Client, user: User) {
+  //should be of type User
+  // should be of type User
+  const resUser: QueryResult<any> = await client
     .query(`insert into allwrite_user (id) values (default) returning *;`)
     .then((resUser) => {
       // console.log("resUser.rows[0]", resUser.rows[0])
@@ -143,7 +130,8 @@ async function createUser(client, user) {
   return resUser.rows;
 }
 
-async function createPage(client, page) {
+export async function createPage(client: Client, page) {
+  // page:Page
   // console.log("service: Page", page);
   const id = await client.query("select id from page order by id desc limit 1");
   // console.log("id", id.rows[0].id);
@@ -160,7 +148,12 @@ async function createPage(client, page) {
   return res.rows;
 }
 
-async function insertUserRatingByUserIdAndPageId(client, user_id, page_id, rating) {
+export async function insertUserRatingByUserIdAndPageId(
+  client: Client,
+  user_id: string,
+  page_id: string,
+  rating: string
+) {
   const res = await client.query(
     `insert into rating (user_id, page_id, rating) values (${user_id}, ${page_id}, ${rating})
     returning *;`
@@ -169,7 +162,7 @@ async function insertUserRatingByUserIdAndPageId(client, user_id, page_id, ratin
   return res.rows;
 }
 
-async function avgRatingByPageId(client, page_id) {
+export async function avgRatingByPageId(client: Client, page_id: string) {
   const res = await client.query(
     `select round(avg(rating),1) as avg_rating, count(rating) as total from rating where page_id = ${page_id}
     ;`
@@ -178,7 +171,12 @@ async function avgRatingByPageId(client, page_id) {
   return res.rows;
 }
 
-async function updateUserRatingByUserIdAndPageId(client, user_id, page_id, rating) {
+export async function updateUserRatingByUserIdAndPageId(
+  client: Client,
+  user_id: string,
+  page_id: string,
+  rating: string
+) {
   const res = await client.query(
     `update rating
     set rating = ${rating}
@@ -190,7 +188,7 @@ async function updateUserRatingByUserIdAndPageId(client, user_id, page_id, ratin
   return res.rows;
 }
 
-async function getRatingByUserAndPage(client, user_id, page_id) {
+export async function getRatingByUserAndPage(client: Client, user_id: string, page_id: string) {
   const res = await client.query(
     `select * from rating where user_id = ${user_id} and page_id = ${page_id}`
   );
@@ -198,7 +196,8 @@ async function getRatingByUserAndPage(client, user_id, page_id) {
   return res.rows;
 }
 
-async function readPage(client, read) {
+export async function readPage(client: Client, read) {
+  // should be of type Read
   const res = await client.query(
     // `insert into user_read_pages (user_id, page_id) values (${read.user_id}, ${read.page_id});`
     `insert into user_read_pages (user_id, page_id)
@@ -212,7 +211,7 @@ async function readPage(client, read) {
   return res.rows;
 }
 
-async function getIfPageRead(client, page_id, user_id) {
+export async function getIfPageRead(client: Client, page_id: string, user_id: string) {
   const res = await client.query(
     `select * from user_read_pages where page_id=${page_id} and user_id=${user_id};`
   );
@@ -220,13 +219,13 @@ async function getIfPageRead(client, page_id, user_id) {
   return res.rows;
 }
 
-async function getCoverById(client, cover_id) {
+export async function getCoverById(client: Client, cover_id: string) {
   const res = await client.query(`select * from covers where id = ${cover_id};`);
   // console.log(res.rows);
   return res.rows;
 }
 
-async function getCoverByPageId(client, page_id) {
+export async function getCoverByPageId(client: Client, page_id: string) {
   const res = await client.query(`with recursive tree AS (
     SELECT n1.id, n1.parent_id, 1 as hlevel
     from page n1
@@ -262,13 +261,13 @@ where tr.parent_id is null)
   return res.rows;
 }
 
-async function getPageById(client, page_id) {
+export async function getPageById(client: Client, page_id: string) {
   const res = await client.query(`select * from page where id = ${page_id}`);
   // console.log(res.rows);
   return res.rows;
 }
 
-async function getBookmarksByUserId(client, user_id) {
+export async function getBookmarksByUserId(client: Client, user_id: string) {
   const res = await client.query(
     `select cb.cover_id as bm_cover_id \
     from cover_bookmarks cb \
@@ -278,7 +277,7 @@ async function getBookmarksByUserId(client, user_id) {
   return res.rows;
 }
 
-async function getCovers(client, limit) {
+export async function getCovers(client: Client, limit: string) {
   const res = await client.query(
     `select c.id as cover_id, c.title, c.author, c.genre, c.image_url, c.summary from covers c order by c.id limit ${limit}`
   );
@@ -286,21 +285,21 @@ async function getCovers(client, limit) {
   return res.rows;
 }
 
-async function getAuthorChildForPage(client, author, p_id) {
+export async function getAuthorChildForPage(client: Client, author: string, p_id: string) {
   const res = await client.query(
     `select * from page where parent_id = ${p_id} and author = '${author}' order by RANDOM()`
   );
   return res.rows;
 }
 
-async function getHighestRatingChoices(client, p_id) {
+export async function getHighestRatingChoices(client: Client, p_id: string) {
   const res = await client.query(
     `select * from page n join rating r on n.id = r.node_id where n.parent_id = ${p_id} order by r.rating desc limit 1`
   );
   return res.rows;
 }
 
-async function getLongestStoryChoicesFrom(client, n_id) {
+export async function getLongestStoryChoicesFrom(client: Client, n_id: string) {
   // console.log("longeststories:pageid:", n_id);
   const res = await client.query(
     `with recursive tree AS (
@@ -350,6 +349,6 @@ module.exports = {
   updateUserRatingByUserIdAndPageId,
   createUser,
   getIfPageRead,
-  login,
+  // login,
   isUsernameTaken,
 };
