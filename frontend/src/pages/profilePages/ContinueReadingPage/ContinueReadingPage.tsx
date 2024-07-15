38b597ed45ff549210323df07a0571ca88b92bb0
Brian <53064162+bmagee88@users.noreply.github.com>
2024-07-15 12:46:59 +0300

    add heading to continue reading page and if no user nav to login

--- frontend/src/pages/profilePages/ContinueReadingPage/ContinueReadingPage.tsx
+++ frontend/src/pages/profilePages/ContinueReadingPage/ContinueReadingPage.tsx
@@ -4,9 +4,13 @@ import MyContributionsDisplay from "./ContinueReadingDisplay/ContinueReadingDisp
 import { RootState } from "../../../common/store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { setBookmarks } from "../../../common/store/continueReading/continueReading";
+import { useNavigate } from "react-router";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 const ContinueReadingPage: React.FC = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   // check if theres data in redux
   // have a manual refresh button or timeout to refetch with updated values
   // consider having a time out for the button so they can't just spam the button
@@ -25,11 +29,19 @@ const ContinueReadingPage: React.FC = () => {
         dispatch(setBookmarks(data.data));
       } else {
         console.log("response not okay");
+        navigate("/login");
       }
     };
     fetchCoversAndPagesByUser(user_id || 0);
-  }, [dispatch, user_id]);
-  return <MyContributionsDisplay bookmarks={bookmarks} />;
+  }, [dispatch, navigate, user_id]);
+  return (
+    <Box>
+      <Box sx={{ paddingLeft: "1rem", paddingTop: ".5rem" }}>
+        <Typography fontSize={24}>Continue Reading</Typography>
+      </Box>
+      <MyContributionsDisplay bookmarks={bookmarks} />
+    </Box>
+  );
 };
 
 export default ContinueReadingPage;
