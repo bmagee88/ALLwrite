6318a119b76509fedd49e73977e0d5f8b84aef23
Brian <53064162+bmagee88@users.noreply.github.com>
2024-07-18 16:33:12 +0300

    added my-contributions endpoint

--- backend/src/routes/user/UserRoute.ts
+++ backend/src/routes/user/UserRoute.ts
@@ -1,6 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { createUser, getContinueReadingByUserId } from "../../services/NodeService.js";
+import {
+  createUser,
+  getContinueReadingByUserId,
+  getContributionsByUserId,
+} from "../../services/NodeService.js";
 import UserDto from "../../dtos/UserDto.js";
 
 router.post("/create-user", (req, res) => {
@@ -32,6 +36,23 @@ router.get("/:user_id/continue-reading", async (req, res) => {
   const client = req.client;
   try {
     const dbResult = await getContinueReadingByUserId(client, user_id);
+    res.status(200).json({ data: dbResult });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/:user_id/my-contributions", async (req, res) => {
+  console.log("in my-contributions endpoint");
+  const user_id = req.params.user_id;
+  if (!user_id) {
+    console.log("bad user id format");
+    res.status(400).json({ data: "bad user id format" });
+    return;
+  }
+  const client = req.client;
+  try {
+    const dbResult = await getContributionsByUserId(client, user_id);
     res.status(200).json({ data: dbResult });
   } catch (err) {
     console.log(err);
