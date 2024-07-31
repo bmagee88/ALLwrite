0bdc6cc7c2b68af58f583080a96ef057c93465c3
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2024-07-31 15:50:29 +0300

    add: utils related to openAI

---
+++ backend/src/routes/openAi/utils.ts
@@ -0,0 +1,7 @@
+export function emotionMod(emotion: string) {
+  return `the text i sent you above is a story or a part of a story, rewrite it in a ${emotion} way`;
+}
+
+// export function authorMod(author: string) {
+//   return `the text i sent you above is a story or a part of a story, rewrite it in the style of ${author}`;
+// }
